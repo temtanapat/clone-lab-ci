c71affbd200759747e971964a03e6eabd47eba69
tOnz <woraponse@gmail.com>
2014-05-06 14:02:21 +0700

    change comment with my name

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENT tOnz
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
